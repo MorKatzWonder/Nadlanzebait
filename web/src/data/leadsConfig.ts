a6c8a1fb381a722a3fa761b714a15065344b8a18
MorKatzWonder <mortzkatz@gmail.com>
2026-09-23 08:27:36 +0300

    Update LEADS_WEBHOOK_URL to new endpoint

--- web/src/data/leadsConfig.ts
+++ web/src/data/leadsConfig.ts
@@ -7,4 +7,4 @@
  * opens WhatsApp exactly as before, nothing breaks.
  */
 export const LEADS_WEBHOOK_URL =
-  "https://script.google.com/macros/s/AKfycbxa0iF2q5wRfnfytQUXqHQyJM-9KUKUScC6T4qhdZkkDCUTCBMnZt2EodlRSV6UeHsqkA/exec";
+  "https://script.google.com/macros/s/AKfycbz8c6tJNqwPioyUcwJHqmvcTQxOZuUVTZnmgBvMm1aVaKZXnzjun3XUbJqGNdu0iEjlkA/exec";
